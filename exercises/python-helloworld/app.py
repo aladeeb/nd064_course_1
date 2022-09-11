ab7f46641e458bef155a8a4a1136b90f3d41be38
Adeeb <ahmed.ayman.aladeeb@gmail.com>
2022-09-12 00:39:38 +0200

    add logging

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask import Response
 from flask import json
+import logging
+
+logging.basicConfig(filename="app.log", level="DEBUG")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    logging.debug("Hello World")
     return "Hello World, Ahmed!"
 
 @app.route("/metrics")
 def metrics():
+    logging.info("This is a metrics request")
     return Response(json.dumps({"UserCount": 140, "UserCountActive": 23}), status=200, mimetype='application/json')
 
 @app.route("/status")
 def status():
+    logging.debug("Someone hit the status API")
     return Response("result: OK - healthy", status=200, mimetype='application/json')
 
 
