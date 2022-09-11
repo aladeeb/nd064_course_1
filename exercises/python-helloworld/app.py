4d6092242350db65803c2622eecb596ea78b9ede
Adeeb <ahmed.ayman.aladeeb@gmail.com>
2022-09-12 00:21:01 +0200

    exercise 1

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask import Response
+from flask import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World, Ahmed!"
+
+@app.route("/metrics")
+def metrics():
+    return Response(json.dumps({"UserCount": 140, "UserCountActive": 23}), status=200, mimetype='application/json')
+
+@app.route("/status")
+def status():
+    return Response("result: OK - healthy", status=200, mimetype='application/json')
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=3000)
